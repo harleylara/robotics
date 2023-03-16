794505b0485abb42f90d5ff8487c2cc41f145fc6
harleylara <harley.lara@outlook.com>
2023-03-17 00:40:16 +0100

    helper parser for markdown and latex used for descriptions

---
+++ src/tools/helper.parser.js
@@ -0,0 +1,29 @@
+import katex from 'katex';
+import { NOTATION } from '../consts';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkMath from 'remark-math';
+import remarkRehype from 'remark-rehype';
+import rehypeKatex from 'rehype-katex';
+import rehypeStringify from 'rehype-stringify';
+
+export const markToHtml = function(text) {
+
+    let renderedText = unified()
+        .use(remarkParse)
+        .use(remarkMath)
+        .use(remarkRehype)
+        .use(rehypeKatex, {
+            macros: NOTATION
+        })
+        .use(rehypeStringify)
+        .process(text)
+
+    return renderedText;
+}
+
+export const latexToHtml = function(latex) {
+    return katex.renderToString(latex, {macros:NOTATION});
+
+}
+
